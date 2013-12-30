000b3e19673e677536a223b4097880ade69ec5ef
amhayslip <amhayslip@gmail.com>
2013-12-30 14:47:28 -0600

    Fix devise edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,14 @@ class User < ActiveRecord::Base
     super && provider.blank?
   end
 
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
+
 
 
 end
