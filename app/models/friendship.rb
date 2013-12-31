6c02ad259b75d0948141db533650f9f307a51e03
amhayslip <amhayslip@gmail.com>
2013-12-31 12:50:40 -0600

    Add mutual friend create methods

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,10 +5,23 @@ class Friendship < ActiveRecord::Base
   state_machine :state, initial: :pending do
     after_transition on: :accept, do: :send_acceptance_email
 
+    state :requested
+
     event :accept do
       transition any => :accepted
     end
   end
+
+  def self.request(user1, user2)
+    transaction do
+      friendship1 = create!(user: user1, friend: user2, state: 'pending')
+      friendship2 = create!(user: user2, friend: user1, state: 'requested')
+
+      friendship1.send_request_email
+      friendship1
+    end 
+  end
+
 
   def send_request_email
     UserNotifier.friend_requested(id).deliver
